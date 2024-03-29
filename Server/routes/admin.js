34344321c8e4bfa9000f687d9975ea1701822557
Umar Aziz <umarocks@hotmail.com>
2024-03-28 22:07:24 -0500

    create admin routes and variables declared, need to add the DB query

--- Server/routes/admin.js
+++ Server/routes/admin.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const pool = require("../db");
-const uuid = require("uuidv4");
+const { v4: uuidv4 } = require("uuid");
+const { hashPassword } = require("../middleware/hash");
+
 const { IsAdmin } = require("../middleware/IsAdmin");
 const {
   ensureAuthenticated,
@@ -9,35 +11,101 @@ const {
 } = require("../middleware/LoginChecker");
 
 // create admin user
-router.post("/createAdmin", ensureAuthenticated, IsAdmin, function (req, res) {
-  res.write("Login Created");
-  // res.write(req.body);
-  res.send();
-});
+router.post(
+  "/createAdmin",
+  ensureAuthenticated,
+  IsAdmin,
+  async function (req, res) {
+    res.write("Login Created");
+    console.log(req.body);
+    const password = hashPassword(req.body.password);
+    const emp_id = "A" + uuidv4();
+    console.log(emp_id);
+    console.log(password);
+    const { name, phone, shift_timings, email } = req.body;
+
+    // const result = await pool.query(
+    //   "INSERT INTO Login (Email,Password,Role) VALUES ($1,$2,$3);",
+    //   [req.body.Email, password, "Admin"]
+    // );
+    // INSERT INTO Login (Email,Password,Role) VALUES ('john@example.com','admin123','Admin');
+
+    // INSERT INTO Admin (Emp_ID, Name, Phone, Shift_Timings, Authorization_Type, Email)
+    // VALUES ('A88494d28-3ef3-45ff-91bf-04a50b6d2d8e', 'John', '1234567890', '9:00 AM - 5:00 PM', 'Admin', 'john@example.com');
+
+    // Admin Schema , Make sure admin has A in the emp_id
+    //       select * from admin;
+    //                 emp_id                 |    name    |   phone    |   shift_timings   | authorization_type |      email
+    // ---------------------------------------+------------+------------+-------------------+--------------------+------------------
+    //  A88494d28-3ef3-45ff-91bf-04a50b6d2d8e | John       | 1234567890 | 9:00 AM - 5:00 PM | Admin              | john@example.com
+
+    res.send();
+  }
+);
 
 //create owner
-router.post("/createOwner", function (req, res) {});
+router.post(
+  "/createOwner",
+  ensureAuthenticated,
+  IsAdmin,
+  async function (req, res) {}
+);
 
 //create tenant
-router.post("/createTenant", function (req, res) {});
+router.post(
+  "/createTenant",
+  ensureAuthenticated,
+  IsAdmin,
+  async function (req, res) {}
+);
 
 //show all tenant detail
-router.get("/getAllTenant", function (req, res) {});
+router.get(
+  "/getAllTenant",
+  ensureAuthenticated,
+  IsAdmin,
+  async function (req, res) {}
+);
 
 //show all owner detail
-router.get("/getAllOwner", function (req, res) {});
+router.get(
+  "/getAllOwner",
+  ensureAuthenticated,
+  IsAdmin,
+  async function (req, res) {}
+);
 
 //allot parking slot
-router.post("/AllotParking", function (req, res) {});
+router.post(
+  "/AllotParking",
+  ensureAuthenticated,
+  IsAdmin,
+  async function (req, res) {}
+);
 
 // view complain
-router.get("/getAllComplaint", function (req, res) {});
+router.get(
+  "/getAllComplaint",
+  ensureAuthenticated,
+  IsAdmin,
+  async function (req, res) {}
+);
 
 // total owner count
-router.get("/totalOwner", function (req, res) {});
+router.get(
+  "/totalOwner",
+  ensureAuthenticated,
+  IsAdmin,
+  async function (req, res) {}
+);
 
 //total tenant count
-router.get("/totalTenant", function (req, res) {});
+router.get(
+  "/totalTenant",
+  ensureAuthenticated,
+  IsAdmin,
+  async function (req, res) {}
+);
 
 //total employee count
 module.exports = router;
