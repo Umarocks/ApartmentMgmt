01b1429c31da4338f8c62df5bed17107f45d025f
Umar Aziz <umarocks@hotmail.com>
2024-03-29 16:50:49 -0500

    added cascading to the parent tables and modified constraints for table increasing their pkey to 38 to accomodate uuid standard

--- Server/database.sql
+++ Server/database.sql
@@ -1,6 +1,6 @@
 -- Create table Owner
 CREATE TABLE Owner (
-    Owner_id VARCHAR(10),
+    Owner_id VARCHAR(38),
     Name VARCHAR(20),
     SSN VARCHAR(9) NOT NULL,
     Phone_no VARCHAR(10) NOT NULL,
@@ -11,26 +11,26 @@ CREATE TABLE Owner (
 
 -- Create table Payment
 CREATE TABLE Payment (
-    Payment_id VARCHAR(20),
+    Payment_id VARCHAR(38),
     Mode VARCHAR(10),
     Payment_date DATE,
     Amount NUMERIC(5,2),
-    Owner_id VARCHAR(10),
-    Tenant_id VARCHAR(10),
+    Owner_id VARCHAR(38),
+    Tenant_id VARCHAR(38),
     PRIMARY KEY (Payment_id)
 );
 
 -- Create table Login
 CREATE TABLE Login (
     Email VARCHAR(50),
-    Password VARCHAR(50) NOT NULL,
+    Password VARCHAR(72) NOT NULL,
     Role VARCHAR(15) NOT NULL,
     PRIMARY KEY (Email)
 );
 
 -- Create table Admin
 CREATE TABLE Admin (
-    Emp_ID VARCHAR(10),
+    Emp_ID VARCHAR(38),
     Name CHAR(10),
     Phone VARCHAR(10),
     Shift_Timings VARCHAR(50),
@@ -41,7 +41,7 @@ CREATE TABLE Admin (
 
 -- Create table Maintenance Staff
 CREATE TABLE Maintenance_Staff (
-    Emp_ID VARCHAR(10),
+    Emp_ID VARCHAR(38),
     Name CHAR(10),
     Phone VARCHAR(10),
     Shift_Timings VARCHAR(50),
@@ -53,7 +53,7 @@ CREATE TABLE Maintenance_Staff (
 
 -- Create table Tenant
 CREATE TABLE Tenant (
-    Tenant_id VARCHAR(10), 
+    Tenant_id VARCHAR(38), 
     Name CHAR(20) NOT NULL, 
     SSN VARCHAR(9) NOT NULL, 
     Age INT,
@@ -65,7 +65,7 @@ CREATE TABLE Tenant (
 
 -- Create table Tenant_Contact
 CREATE TABLE Tenant_Contact (
-    Tenant_id VARCHAR(10), 
+    Tenant_id VARCHAR(38), 
     Phone VARCHAR(10), 
     PRIMARY KEY (Tenant_id, Phone)
 );
@@ -87,8 +87,10 @@ CREATE TABLE Apartment (
     Area INT NOT NULL, 
     Floor INT NOT NULL, 
     Address VARCHAR(50) NOT NULL, 
-    Owner_id VARCHAR(10), 
-    PRIMARY KEY (Apt_No, Block_id)
+    Owner_id VARCHAR(38), 
+    -- PRIMARY KEY (Apt_No, Block_id)
+    PRIMARY KEY (Apt_No)
+
 );
 
 -- Create table Parking
@@ -96,31 +98,31 @@ CREATE TABLE Parking (
     Spot_no INT, 
     Type VARCHAR(10), 
     Block_id INT, 
-    Tenant_id VARCHAR(10), 
+    Tenant_id VARCHAR(38), 
     PRIMARY KEY (Spot_no)
 );
 
 -- Create table Manages
 CREATE TABLE Manages (
-    Emp_ID VARCHAR(10), 
+    Emp_ID VARCHAR(38), 
     Block_id INT, 
     PRIMARY KEY (Emp_ID, Block_id)
 );
 
 -- Create table Complaint
 CREATE TABLE Complaint (
-    Complaint_ID VARCHAR(20),
+    Complaint_ID VARCHAR(38),
     Complaint_description VARCHAR(100) NOT NULL,
     Complaint_date DATE NOT NULL,
-    Emp_ID VARCHAR(10),
-    Tenant_id VARCHAR(10),
+    Emp_ID VARCHAR(38),
+    Tenant_id VARCHAR(38),
     PRIMARY KEY (Complaint_ID)
 );
 
 -- Create table Rents
 CREATE TABLE Rents (
-    Tenant_id VARCHAR(10), 
-    Owner_id VARCHAR(10), 
+    Tenant_id VARCHAR(38), 
+    Owner_id VARCHAR(38), 
     Rent_amount NUMERIC(5,2) NOT NULL,  
     PRIMARY KEY(Tenant_id, Owner_id)  
 );
@@ -131,32 +133,200 @@ CREATE TABLE Apartment_application (
     Phone VARCHAR(10) NOT NULL,
     Name CHAR(20),  
     Apt_address VARCHAR(50),  
-    Owner_id VARCHAR(10),  
+    Owner_id VARCHAR(38),  
     PRIMARY KEY(Email)  
 );
 
 -- Add foreign key constraints using ALTER TABLE statements
-ALTER TABLE Owner ADD CONSTRAINT fk_owner_login FOREIGN KEY (Email) REFERENCES Login;
-ALTER TABLE Payment ADD CONSTRAINT fk_payment_owner FOREIGN KEY (Owner_id) REFERENCES Owner;
-ALTER TABLE Payment ADD CONSTRAINT fk_payment_tenant FOREIGN KEY (Tenant_id) REFERENCES Tenant;
-ALTER TABLE Admin ADD CONSTRAINT fk_admin_login FOREIGN KEY (Email) REFERENCES Login;
-ALTER TABLE Maintenance_Staff ADD CONSTRAINT fk_maintenance_staff_login FOREIGN KEY (Email) REFERENCES Login;
-ALTER TABLE Tenant ADD CONSTRAINT fk_tenant_login FOREIGN KEY (Email) REFERENCES Login;
-ALTER TABLE Tenant ADD CONSTRAINT fk_tenant_apartment FOREIGN KEY (Apt_no) REFERENCES Apartment;
-ALTER TABLE Tenant_Contact ADD CONSTRAINT fk_tenant_contact FOREIGN KEY (Tenant_id) REFERENCES Tenant;
-ALTER TABLE Apartment ADD CONSTRAINT fk_apartment_block FOREIGN KEY (Block_id) REFERENCES Block;
-ALTER TABLE Apartment ADD CONSTRAINT fk_apartment_owner FOREIGN KEY (Owner_id) REFERENCES Owner;
-ALTER TABLE Parking ADD CONSTRAINT fk_parking_block FOREIGN KEY (Block_id) REFERENCES Block;
-ALTER TABLE Parking ADD CONSTRAINT fk_parking_tenant FOREIGN KEY (Tenant_id) REFERENCES Tenant;
-ALTER TABLE Manages ADD CONSTRAINT fk_manages_admin FOREIGN KEY (Emp_ID) REFERENCES Admin;
-ALTER TABLE Manages ADD CONSTRAINT fk_manages_block FOREIGN KEY (Block_id) REFERENCES Block;
-ALTER TABLE Complaint ADD CONSTRAINT fk_complaint_maintenance_staff FOREIGN KEY (Emp_ID) REFERENCES Maintenance_Staff;
-ALTER TABLE Complaint ADD CONSTRAINT fk_complaint_tenant FOREIGN KEY (Tenant_id) REFERENCES Tenant;
-ALTER TABLE Rents ADD CONSTRAINT fk_rents_tenant FOREIGN KEY (Tenant_id) REFERENCES Tenant;
-ALTER TABLE Rents ADD CONSTRAINT fk_rents_owner FOREIGN KEY (Owner_id) REFERENCES Owner;
-ALTER TABLE Apartment_application ADD CONSTRAINT fk_apartment_application_login FOREIGN KEY (Email) REFERENCES Login;
-ALTER TABLE Apartment_application ADD CONSTRAINT fk_apartment_application_owner FOREIGN KEY (Owner_id) REFERENCES Owner;
+ALTER TABLE Owner ADD CONSTRAINT fk_owner_login FOREIGN KEY (Email) REFERENCES Login ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Payment ADD CONSTRAINT fk_payment_owner FOREIGN KEY (Owner_id) REFERENCES Owner ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Payment ADD CONSTRAINT fk_payment_tenant FOREIGN KEY (Tenant_id) REFERENCES Tenant ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Admin ADD CONSTRAINT fk_admin_login FOREIGN KEY (Email) REFERENCES Login ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Maintenance_Staff ADD CONSTRAINT fk_maintenance_staff_login FOREIGN KEY (Email) REFERENCES Login ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Tenant ADD CONSTRAINT fk_tenant_login FOREIGN KEY (Email) REFERENCES Login ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Tenant ADD CONSTRAINT fk_tenant_apartment FOREIGN KEY (Apt_no) REFERENCES Apartment ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Tenant_Contact ADD CONSTRAINT fk_tenant_contact FOREIGN KEY (Tenant_id) REFERENCES Tenant ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Apartment ADD CONSTRAINT fk_apartment_block FOREIGN KEY (Block_id) REFERENCES Block ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Apartment ADD CONSTRAINT fk_apartment_owner FOREIGN KEY (Owner_id) REFERENCES Owner ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Parking ADD CONSTRAINT fk_parking_block FOREIGN KEY (Block_id) REFERENCES Block ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Parking ADD CONSTRAINT fk_parking_tenant FOREIGN KEY (Tenant_id) REFERENCES Tenant ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Manages ADD CONSTRAINT fk_manages_admin FOREIGN KEY (Emp_ID) REFERENCES Admin ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Manages ADD CONSTRAINT fk_manages_block FOREIGN KEY (Block_id) REFERENCES Block ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Complaint ADD CONSTRAINT fk_complaint_maintenance_staff FOREIGN KEY (Emp_ID) REFERENCES Maintenance_Staff ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Complaint ADD CONSTRAINT fk_complaint_tenant FOREIGN KEY (Tenant_id) REFERENCES Tenant ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Rents ADD CONSTRAINT fk_rents_tenant FOREIGN KEY (Tenant_id) REFERENCES Tenant ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Rents ADD CONSTRAINT fk_rents_owner FOREIGN KEY (Owner_id) REFERENCES Owner ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Apartment_application ADD CONSTRAINT fk_apartment_application_login FOREIGN KEY (Email) REFERENCES Login ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE Apartment_application ADD CONSTRAINT fk_apartment_application_owner FOREIGN KEY (Owner_id) REFERENCES Owner ON UPDATE CASCADE ON DELETE CASCADE;
 
+
+--Alter table to add cascade constraints
+ALTER TABLE Owner
+DROP CONSTRAINT IF EXISTS fk_owner_login,
+ADD CONSTRAINT fk_owner_login
+FOREIGN KEY (Email) 
+REFERENCES Login 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Payment
+DROP CONSTRAINT IF EXISTS fk_payment_owner,
+ADD CONSTRAINT fk_payment_owner
+FOREIGN KEY (Owner_id) 
+REFERENCES Owner 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Payment
+DROP CONSTRAINT IF EXISTS fk_payment_tenant,
+ADD CONSTRAINT fk_payment_tenant
+FOREIGN KEY (Tenant_id) 
+REFERENCES Tenant 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Admin
+DROP CONSTRAINT IF EXISTS fk_admin_login,
+ADD CONSTRAINT fk_admin_login
+FOREIGN KEY (Email) 
+REFERENCES Login 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Maintenance_Staff
+DROP CONSTRAINT IF EXISTS fk_maintenance_staff_login,
+ADD CONSTRAINT fk_maintenance_staff_login
+FOREIGN KEY (Email) 
+REFERENCES Login 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Tenant
+DROP CONSTRAINT IF EXISTS fk_tenant_login,
+ADD CONSTRAINT fk_tenant_login
+FOREIGN KEY (Email) 
+REFERENCES Login 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Tenant
+DROP CONSTRAINT IF EXISTS fk_tenant_apartment,
+ADD CONSTRAINT fk_tenant_apartment
+FOREIGN KEY (Apt_no) 
+REFERENCES Apartment 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Tenant_Contact
+DROP CONSTRAINT IF EXISTS fk_tenant_contact,
+ADD CONSTRAINT fk_tenant_contact
+FOREIGN KEY (Tenant_id) 
+REFERENCES Tenant 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Apartment
+DROP CONSTRAINT IF EXISTS fk_apartment_block,
+ADD CONSTRAINT fk_apartment_block
+FOREIGN KEY (Block_id) 
+REFERENCES Block 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Apartment
+DROP CONSTRAINT IF EXISTS fk_apartment_owner,
+ADD CONSTRAINT fk_apartment_owner
+FOREIGN KEY (Owner_id) 
+REFERENCES Owner 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Parking
+DROP CONSTRAINT IF EXISTS fk_parking_block,
+ADD CONSTRAINT fk_parking_block
+FOREIGN KEY (Block_id) 
+REFERENCES Block 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Parking
+DROP CONSTRAINT IF EXISTS fk_parking_tenant,
+ADD CONSTRAINT fk_parking_tenant
+FOREIGN KEY (Tenant_id) 
+REFERENCES Tenant 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Manages
+DROP CONSTRAINT IF EXISTS fk_manages_admin,
+ADD CONSTRAINT fk_manages_admin
+FOREIGN KEY (Emp_ID) 
+REFERENCES Admin 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Manages
+DROP CONSTRAINT IF EXISTS fk_manages_block,
+ADD CONSTRAINT fk_manages_block
+FOREIGN KEY (Block_id) 
+REFERENCES Block 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Complaint
+DROP CONSTRAINT IF EXISTS fk_complaint_maintenance_staff,
+ADD CONSTRAINT fk_complaint_maintenance_staff
+FOREIGN KEY (Emp_ID) 
+REFERENCES Maintenance_Staff 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Complaint
+DROP CONSTRAINT IF EXISTS fk_complaint_tenant,
+ADD CONSTRAINT fk_complaint_tenant
+FOREIGN KEY (Tenant_id) 
+REFERENCES Tenant 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Rents
+DROP CONSTRAINT IF EXISTS fk_rents_tenant,
+ADD CONSTRAINT fk_rents_tenant
+FOREIGN KEY (Tenant_id) 
+REFERENCES Tenant 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Rents
+DROP CONSTRAINT IF EXISTS fk_rents_owner,
+ADD CONSTRAINT fk_rents_owner
+FOREIGN KEY (Owner_id) 
+REFERENCES Owner 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Apartment_application
+DROP CONSTRAINT IF EXISTS fk_apartment_application_login,
+ADD CONSTRAINT fk_apartment_application_login
+FOREIGN KEY (Email) 
+REFERENCES Login 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+ALTER TABLE Apartment_application
+DROP CONSTRAINT IF EXISTS fk_apartment_application_owner,
+ADD CONSTRAINT fk_apartment_application_owner
+FOREIGN KEY (Owner_id) 
+REFERENCES Owner 
+ON UPDATE CASCADE 
+ON DELETE CASCADE;
+
+--Check all non cascade foreign keys
+SELECT conname AS constraint_name,
+       conrelid::regclass AS table
+FROM pg_constraint
+WHERE contype = 'f'
+  AND confdeltype <> 'c';
 
 -- Adding New Admin Manually
 INSERT INTO Login (Email,Password,Role) VALUES ('john@example.com','admin123','Admin');
@@ -165,3 +335,4 @@ INSERT INTO Admin (Emp_ID, Name, Phone, Shift_Timings, Authorization_Type, Email
 VALUES ('A88494d28-3ef3-45ff-91bf-04a50b6d2d8e', 'John', '1234567890', '9:00 AM - 5:00 PM', 'Admin', 'john@example.com');
 
 -- ADD a new table named signups that admin can approve for login and correctly assign the role;
+
