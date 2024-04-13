5b91f2a1cf41a5bd117f2b0d2340539f8d0a0d9e
shriyajaknalli <shriya.jaknalli@gmail.com>
2024-04-13 01:47:40 -0500

    Owner/Tenant Middlewear

---
+++ Server/middleware/IsOwner.js
@@ -0,0 +1,8 @@
+const IsOwner = (req, res, next) => {
+    if (req.isAuthenticated() && req.user.role === 'Owner') {
+      return next();
+    }
+    return res.status(403).send('Access denied');
+  };
+  
+  module.exports = { IsOwner };
