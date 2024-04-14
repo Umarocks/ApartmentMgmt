a7a386cac9c7ef28dcaf60c7296e76b4e33d81bc
Umar Aziz <umarocks@hotmail.com>
2024-04-14 16:42:09 -0500

    bug fix

--- Server/middleware/IsAdminorOwner.js
+++ Server/middleware/IsAdminorOwner.js
@@ -1,8 +1,13 @@
 const IsAdminOrOwner = (req, res, next) => {
-    if (req.isAuthenticated() && (req.user.role === 'Admin' || req.user.role === 'Owner')) {
-      return next();
-    }
-    res.status(403).send('Access Denied: You do not have permission to perform this action.');
-  };
-  
-  module.exports = { IsAdminorOwner };
+  if (
+    req.isAuthenticated() &&
+    (req.user.role === "Admin" || req.user.role === "Owner")
+  ) {
+    return next();
+  }
+  res
+    .status(403)
+    .send("Access Denied: You do not have permission to perform this action.");
+};
+
+module.exports = { IsAdminOrOwner };
