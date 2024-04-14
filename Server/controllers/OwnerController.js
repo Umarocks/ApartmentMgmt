9a69b4bff365c470d868a70a4ba995f8f78a08e6
shriyajaknalli <shriya.jaknalli@gmail.com>
2024-04-14 15:16:54 -0500

    adminorowner operation

--- Server/controllers/OwnerController.js
+++ Server/controllers/OwnerController.js
@@ -3,18 +3,40 @@ const { v4: uuidv4 } = require("uuid");
 const { hashPassword } = require("../middleware/hash");
 
 const OwnerController = {
+  // viewProperties: async (req, res) => {
+  //   try {
+  //     const result = await pool.query(
+  //       "SELECT * FROM Apartment WHERE owner_id = $1",
+  //       [req.user.owner_id]
+  //     );
+  //     res.json(result.rows);
+  //   } catch (error) {
+  //     console.error("Error fetching properties", error.stack);
+  //     res.status(500).send("Error fetching properties");
+  //   }
+  // },
+
   viewProperties: async (req, res) => {
-    try {
-      const result = await pool.query(
-        "SELECT * FROM Apartment WHERE owner_id = $1",
-        [req.user.owner_id]
-      );
-      res.json(result.rows);
-    } catch (error) {
-      console.error("Error fetching properties", error.stack);
-      res.status(500).send("Error fetching properties");
+    let query;
+    let queryParams;
+
+    if (req.user.role === 'Admin') {
+        query = 'SELECT * FROM apartments;';
+        queryParams = [];
+    } else if (req.user.role === 'Owner') {
+        query = 'SELECT * FROM apartments WHERE owner_id = $1;';
+        queryParams = [req.user.owner_id];
     }
-  },
+
+    try {
+        const result = await pool.query(query, queryParams);
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error fetching properties', error.stack);
+        res.status(500).send('Error fetching properties');
+    }
+   },
+
 
   respondToComplaint: async (req, res) => {
     // Implementation for responding to a complaint or some other use case
