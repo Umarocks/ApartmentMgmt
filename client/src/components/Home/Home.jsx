998d3b55a8eeb778a6c4de689260cf459f03c352
shriyajaknalli <shriya.jaknalli@gmail.com>
2024-04-17 23:07:33 -0500

    Home page

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Sidebar from '../Sidebar/Sidebar';
+import backgroundImage from '../../Assets/Homepage.jpeg'; 
+
+const Home = () => {
+  return (
+    <div>
+        <Sidebar/>
+    </div>
+  );
+}
+
+export default Home;
