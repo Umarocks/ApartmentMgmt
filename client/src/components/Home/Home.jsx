5b475353c6e3ddffd4010f8958c8e1c57e74f843
aashleshav06 <164956439+aashleshav06@users.noreply.github.com>
2024-04-19 03:23:09 -0500

    Update Home.jsx

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ import backgroundImage from '../../Assets/Homepage.jpeg';
 const Home = () => {
   return (
     <div>
+        <div className="bg-cover bg-center dark:bg-zinc-800" style="background-image: url('https://api.deepai.org/job-view-file/e4a535e9-b1a5-4a8c-82b8-4d9a80f9ee9d/outputs/output.jpg');">
         <Sidebar/>
         <img src="https://bcassetcdn.com/public/blog/wp-content/uploads/2023/07/28140224/Urban-City-Apartment-by-marcololstudio.png" alt="ALMS Logo" className="mx-auto my-4">
         <div className="flex-1 p-10">
@@ -14,9 +15,8 @@ const Home = () => {
                         <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Contact Us</button>
                         <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Login</button>
                     </div>
-                </div>
-
-    </div>
+                </div>  
+        </div>
   );
 }
 
