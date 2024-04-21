936bd3314d6e2516df21f62b96b33b5a7cf0ef09
Umar Aziz <umarocks@hotmail.com>
2024-04-21 03:31:30 -0500

    refactor code

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,45 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './Sidebar.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faMoneyBill, faExclamationTriangle, faCheck } from '@fortawesome/free-solid-svg-icons';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Sidebar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faMoneyBill,
+  faExclamationTriangle,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import SideBarList from "../SideBarList/SideBarList";
 const Sidebar = () => {
   return (
     <div className="home">
-        {/* <h1>Welcome to Apartment Management System</h1> */}
-        <div className="sidebar">
-        <h2 className='logo'>Apartment.co</h2>
-              <ul>
-                   <li className='login'>
-                <Link to="/login">
-                <FontAwesomeIcon icon={faUser} />
-                <span style={{ marginLeft: '8px' }}>Login</span>
-                </Link>
-            </li>
-            <li className='signup'>
-                <Link to="/signup">
-                <FontAwesomeIcon icon={faUser} />
-                <span style={{ marginLeft: '8px' }}>Sign Up</span>
-                </Link>
-            </li>
-            <li className='payrent'>
-                <Link to="/pay-rent">
-                <FontAwesomeIcon icon={faMoneyBill} />
-                <span style={{ marginLeft: '8px' }}>Pay Rent</span>
-                </Link>
-            </li>
-            <li className='filecomplaint'>
-                <Link to="/file-complaint">
-                <FontAwesomeIcon icon={faCheck}/>
-                <span style={{ marginLeft: '8px' }}>File Complaint</span>
-                </Link>
-            </li>
+      {/* <h1>Welcome to Apartment Management System</h1> */}
+      <div className="sidebar">
+        <h2 className="logo">Apartment.co</h2>
+        <ul>
+          <SideBarList link="/login" faValue={faUser} value="Login" />
+          <SideBarList link="/signup" faValue={faUser} value="Sign Up" />
+          <SideBarList
+            link="/pay-rent"
+            faValue={faMoneyBill}
+            value="Pay Rent"
+          />
+          <SideBarList
+            link="/file-complaint"
+            faValue={faCheck}
+            value="File Complaint"
+          />
         </ul>
-        </div>
-  </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
